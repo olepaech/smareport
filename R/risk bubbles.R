fc3ca0645f89cd1dbb2cf80bec8bbe6e4e8cc265
Ole Paech <ole.paech@gmx.de>
2025-07-28 12:13:12 +0200

    function adjustment

--- R/risk bubbles.R
+++ R/risk bubbles.R
@@ -25,10 +25,10 @@ static_risk_bubble <- function(data,
                                xlab = "Risk Type",
                                ylab = "Avg. Importance (0 = low, 4 = high)",
                                title = "Upside and Downside Risk Composition") {
-  
+
   risks_up <- stringr::str_remove(names(data)[upside], "\\s*2$")
   risks_down <- stringr::str_remove(names(data)[downside], "\\s*2$")
-  
+
   mapping <- c(
     "Absolutely no relevance" = 0,
     "Not so Important" = 1,
@@ -36,7 +36,7 @@ static_risk_bubble <- function(data,
     "Important" = 3,
     "Very Important" = 4
   )
-  
+
   df_clean <- data |>
     dplyr::mutate(
       dplyr::across(
@@ -45,7 +45,7 @@ static_risk_bubble <- function(data,
         .names = "num_{.col}"
       )
     )
-  
+
   summary_up <- df_clean |>
     dplyr::select(dplyr::all_of(paste0("num_", risks_up))) |>
     tidyr::pivot_longer(cols = dplyr::everything(), names_to = "category", values_to = "value") |>
@@ -55,7 +55,7 @@ static_risk_bubble <- function(data,
       category = stringr::str_remove(category, "num_"),
       group = "Upside"
     )
-  
+
   summary_down <- df_clean |>
     dplyr::select(dplyr::all_of(paste0("num_", risks_down))) |>
     tidyr::pivot_longer(cols = dplyr::everything(), names_to = "category", values_to = "value") |>
@@ -65,21 +65,21 @@ static_risk_bubble <- function(data,
       category = stringr::str_remove(category, "num_"),
       group = "Downside"
     )
-  
+
   summary_combined <- dplyr::bind_rows(summary_up, summary_down)
   summary_combined$category <- factor(summary_combined$category, levels = unique(summary_combined$category))
-  
+
   my_colors <- c(
     "#1c355e", "#0067ab", "#cce1ee", "#a5835a",
     "#74253e", "#00594f", "#d15f27", "#c7932c"
   )
-  
+
   plot <- ggplot2::ggplot(summary_combined, ggplot2::aes(
     x = group,
     y = avg_importance,
     color = category
   )) +
-    ggplot2::geom_point(size = 6, alpha = 0.8, position = ggplot2::position_jitter(width = 0.2)) +
+    ggplot2::geom_point(size = 6, alpha = 0.8, position = ggplot2::position_dodge(width = 0)) +
     ggplot2::scale_color_manual(values = my_colors) +
     ggplot2::labs(
       title = title,
@@ -92,6 +92,6 @@ static_risk_bubble <- function(data,
       axis.text.x = ggplot2::element_text(size = 12),
       legend.position = "right"
     )
-  
+
   return(plot)
 }
